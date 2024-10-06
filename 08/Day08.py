493837488b5dc20528740a8571029b49c6b0325c
morganb27 <morgan.ballouk@gmail.com>
2024-10-06 16:28:39 +0200

    Day 8 part 2

--- 08/Day08.py
+++ 08/Day08.py
@@ -1,15 +1,17 @@
 import fileinput
 
 PUZZLE = [line.strip() for line in fileinput.input()]
-PART_1 = 0
+PART_1, PART_2 = 0, float("-inf")
 
 def treetop(data):
-    global PART_1
+    global PART_1, PART_2
     rowNum, colNum = len(data), len(data[0])
     for row in range(len(data)):
         for col in range(len(data[row])):
             if isTreeVisible(data, data[row][col], row, col, rowNum, colNum):
                 PART_1 += 1
+            PART_2 = max(PART_2, calculateScenicScore(data, data[row][col], row, col, rowNum, colNum))
+
 
 def isTreeVisible(data, tree, row, col, rowNum, colNum):
     if row == 0 or row == rowNum - 1 or col == 0 or col == colNum - 1:
@@ -18,6 +20,39 @@ def isTreeVisible(data, tree, row, col, rowNum, colNum):
         return True
     elif all(tree > data[i][col] for i in range(row)) or all(tree > data[i][col] for i in range(row + 1, rowNum)):
         return True
+
+def calculateScenicScore(data, tree, row, col, rowNum, colNum):
+    top, right, bottom, left = 0, 0, 0, 0
+    for i in range(row - 1, -1, -1):
+        if data[i][col] < data[row][col]:
+            top += 1
+        elif data[i][col] >= data[row][col]:
+            top += 1
+            break
+
+    for j in range(col + 1, colNum):
+        if data[row][j] < data[row][col]:
+            right += 1
+        elif data[row][j] >= data[row][col]:
+            right += 1
+            break
+    
+    for i in range(row + 1, rowNum):
+        if data[i][col] < data[row][col]:
+            bottom += 1
+        elif data[i][col] >= data[row][col]:
+            bottom += 1
+            break
+    
+    for j in range(col - 1, -1, -1):
+        if data[row][j] < data[row][col]:
+            left += 1
+        elif data[row][j] >= data[row][col]:
+            left += 1
+            break
+    
+    print(data[row][col], top, right, bottom, left)
+    return top * right * bottom * left
     
 
 
@@ -25,3 +60,4 @@ def isTreeVisible(data, tree, row, col, rowNum, colNum):
 treetop(PUZZLE)
 
 print(f"Solution to part 1 is: {PART_1}")
+print(f"Solution to part 2 is: {PART_2}")
